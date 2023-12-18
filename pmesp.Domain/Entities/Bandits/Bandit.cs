b07081fb9419b97cbac60894c73f3d5147b39fd3
Eduardo Franco <franc0edu24@gmail.com>
2023-12-18 11:11:37 -0300

    alterado modelos e DTOs

--- pmesp.Domain/Entities/Bandits/Bandit.cs
+++ pmesp.Domain/Entities/Bandits/Bandit.cs
@@ -7,60 +7,73 @@ public class Bandit
 {
     public string Id { get; private set; }
     public string Name { get; private set; }
-    public string Description { get; private set; }
+    public string? Description { get; private set; }
     public string CPF { get; private set; }
-    public DateTime Birthday { get; private set; }
-    public string Phone { get; private set; }
-    public string Email { get; private set; }
-    public string Surname { get; private set; }
-    public float Weight { get; private set; }
-    public float Height { get; private set; }
+    public DateTime? Birthday { get; private set; }
+    public string? Phone { get; private set; }
+    public string? Email { get; private set; }
+    public string? Surname { get; private set; }
+    public float? Weight { get; private set; }
+    public float? Height { get; private set; }
     public ICollection<RG> rGs { get; private set; }
     public Bandit(
         string id,
         string name,
-        string description,
+        string? description,
         string cPF,
-        DateTime birthday,
-        string phone,
-        string email,
-        string surname,
-        float weight,
-        float height)
+        DateTime? birthday,
+        string? phone,
+        string? email,
+        string? surname,
+        float? weight,
+        float? height)
     {
         Id = id;
+        rGs = new List<RG>();
         ValidateDomain(name, description, cPF, birthday, phone, email, surname, weight, height);
     }
 
     public void Update(string name,
-        string description,
+        string? description,
         string cPF,
-        DateTime birthday,
-        string phone,
-        string email,
-        string surname,
-        float weight,
-        float height)
+        DateTime? birthday,
+        string? phone,
+        string? email,
+        string? surname,
+        float? weight,
+        float? height)
     {
         ValidateDomain(name, description, cPF, birthday, phone, email, surname, weight, height);
     }
 
     public void ValidateDomain(string name,
-        string description,
+        string? description,
         string cPF,
-        DateTime birthday,
-        string phone,
-        string email,
-        string surname,
-        float weight,
-        float height)
+        DateTime? birthday,
+        string? phone,
+        string? email,
+        string? surname,
+        float? weight,
+        float? height)
     {
         DomainExceptionValidation.When(name.Length > 30, "O nome não pode ultrapassar os 30 caracteres");
-        DomainExceptionValidation.When(description.Length > 255, "A descrição não pode ultrapassar os 255 caracteres");
+        if(description != null)
+        {
+            DomainExceptionValidation.When(description.Length > 255, "A descrição não pode ultrapassar os 255 caracteres");
+        }
         DomainExceptionValidation.When(cPF.Length > 14, "O CPF não pode ultrapssar caracteres");
-        DomainExceptionValidation.When(birthday > DateTime.Today, "A data de nascimento não pode ser maior que a atual");
-        DomainExceptionValidation.When(phone.Length > 12, "O telefone não pode ultrapassar os 12 caracteres");
-        DomainExceptionValidation.When(!email.Contains("@"), "O email não tem o formato correto");
+        if(birthday != null)
+        {
+            DomainExceptionValidation.When(birthday > DateTime.Today, "A data de nascimento não pode ser maior que a atual");
+        }
+        if(phone != null)
+        {
+            DomainExceptionValidation.When(phone.Length > 12, "O telefone não pode ultrapassar os 12 caracteres");
+        }
+        if (email != null)
+        {
+            DomainExceptionValidation.When(!email.Contains("@"), "O email não tem o formato correto");
+        }
 
         Name = name;
         Description = description;
---
+++ pmesp.Infrastructure/Migrations/20231216120531_updateingDataBase.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace pmesp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateingDataBase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Sender",
+                table: "RGs",
+                type: "varchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(15)",
+                oldMaxLength: 15)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Weight",
+                table: "Bandits",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(float),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Surname",
+                table: "Bandits",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Phone",
+                table: "Bandits",
+                type: "varchar(12)",
+                maxLength: 12,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(12)",
+                oldMaxLength: 12)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Height",
+                table: "Bandits",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(float),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Bandits",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Bandits",
+                type: "varchar(255)",
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)",
+                oldMaxLength: 255)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Birthday",
+                table: "Bandits",
+                type: "datetime(6)",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Sender",
+                table: "RGs",
+                type: "varchar(15)",
+                maxLength: 15,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldMaxLength: 50)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Weight",
+                table: "Bandits",
+                type: "float",
+                nullable: false,
+                defaultValue: 0f,
+                oldClrType: typeof(float),
+                oldType: "float",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Bandits",
+                keyColumn: "Surname",
+                keyValue: null,
+                column: "Surname",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Surname",
+                table: "Bandits",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Bandits",
+                keyColumn: "Phone",
+                keyValue: null,
+                column: "Phone",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Phone",
+                table: "Bandits",
+                type: "varchar(12)",
+                maxLength: 12,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(12)",
+                oldMaxLength: 12,
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Height",
+                table: "Bandits",
+                type: "float",
+                nullable: false,
+                defaultValue: 0f,
+                oldClrType: typeof(float),
+                oldType: "float",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Bandits",
+                keyColumn: "Email",
+                keyValue: null,
+                column: "Email",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Bandits",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Bandits",
+                keyColumn: "Description",
+                keyValue: null,
+                column: "Description",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Bandits",
+                type: "varchar(255)",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)",
+                oldMaxLength: 255,
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Birthday",
+                table: "Bandits",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)",
+                oldNullable: true);
+        }
+    }
+}
