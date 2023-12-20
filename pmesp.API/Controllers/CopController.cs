8bad705e8cd616195d288f6fb110faba73daee72
Eduardo Franco <franc0edu24@gmail.com>
2023-12-20 14:46:57 -0300

    login funcionando

--- pmesp.API/Controllers/CopController.cs
+++ pmesp.API/Controllers/CopController.cs
@@ -16,32 +16,19 @@ namespace pmesp.API.Controllers;
 public class CopController : ControllerBase
 {
     private readonly ICopService _copService;
-    private readonly IAuthenticate _authenticateService;
 
     public CopController(
-        ICopService copService,
-        IAuthenticate authenticateService
+        ICopService copService
     )
     {
         _copService = copService;
-        _authenticateService = authenticateService;
     }
 
     [HttpPost]
-    public async Task<ActionResult<TokenDTO>> CreateAccountCop([FromBody] CopDTO cop)
+    public async Task<ActionResult<CopDTO>> CreateAccountCop([FromBody] CopDTO cop)
     {
         var result =  await _copService.PostAsync(cop);
-        
-        if(result.Success)
-        {
-            var token = _authenticateService.GenerateToken(cop.Id, cop.Email);
-            return new TokenDTO
-            {
-                TokenJWT = token,
-            };
-        }
-
-        return BadRequest(result);
+        return result.Success ? Ok(result) : BadRequest(result);
     }
 
     [HttpPost("login")]
